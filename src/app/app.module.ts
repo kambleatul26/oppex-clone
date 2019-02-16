ce22e81a3bc8431afa8cea2cd0628e7ae5045548
Atul Kamble <atulkamble@localhost.localdomain>
2019-02-16 20:53:30 +0530

    routing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
+import { SearchComponent } from './search/search.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    SearchComponent,
+    PricingComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home', component: BodyComponent},
+      {path: 'search', component: SearchComponent},
+      {path: 'pricing', component: PricingComponent},
+      {path: 'signup', component: LoginComponent},
+      {path: '**', component: BodyComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
