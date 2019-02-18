7fd2bbda6483659e7d09310ac575e4cde549d3e5
Atul Kamble <atulkamble@localhost.localdomain>
2019-02-18 06:54:56 +0530

    Revert "routing done"
    This reverts commit ce22e81a3bc8431afa8cea2cd0628e7ae5045548.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
-import { SearchComponent } from './search/search.component';
-import { PricingComponent } from './pricing/pricing.component';
-import { LoginComponent } from './login/login.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent,
-    SearchComponent,
-    PricingComponent,
-    LoginComponent
+    BodyComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-      {path: 'home', component: BodyComponent},
-      {path: 'search', component: SearchComponent},
-      {path: 'pricing', component: PricingComponent},
-      {path: 'signup', component: LoginComponent},
-      {path: '**', component: BodyComponent},
-    ])
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
